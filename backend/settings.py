73ac83b76d6a94ba4d98bf343642436ea2685661
Jerome Infante <jeromeinfante.dev@gmail.com>
2025-12-18 08:41:31 +0800

    added allowed host on settings

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '*')]
 
 
 # Application definition
